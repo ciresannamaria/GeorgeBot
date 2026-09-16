cc36532044852c36770d5675607141d39f816b5e
cires.annamaria <63457648+ciresannamaria@users.noreply.github.com>
2026-09-16 09:20:21 +0200

    Add four Year 1-2 Coding Games activities

--- activities.js
+++ activities.js
@@ -14,6 +14,42 @@ const activities = [
         link: "https://codejr.org/"
     },
     {
+        title: "Learn to Drag and Drop",
+        description: "",
+        icon: "🖱️",
+        category: "Coding Games",
+        yearGroups: ["years1-2"],
+        type: "external",
+        link: "https://studio.code.org/courses/coursea-2025/units/1/lessons/2/levels/1?viewAs=Instructor"
+    },
+    {
+        title: "Sequencing with Scrat",
+        description: "",
+        icon: "🧩",
+        category: "Coding Games",
+        yearGroups: ["years1-2"],
+        type: "external",
+        link: "https://studio.code.org/courses/coursea-2025/units/1/lessons/4/levels/2?viewAs=Instructor"
+    },
+    {
+        title: "Programming with Scrat",
+        description: "",
+        icon: "🐿️",
+        category: "Coding Games",
+        yearGroups: ["years1-2"],
+        type: "external",
+        link: "https://studio.code.org/courses/coursea-2025/units/1/lessons/5/levels/2?viewAs=Instructor"
+    },
+    {
+        title: "Programming with Rey and BB-8",
+        description: "",
+        icon: "🤖",
+        category: "Coding Games",
+        yearGroups: ["years1-2"],
+        type: "external",
+        link: "https://studio.code.org/courses/coursea-2025/units/1/lessons/6/levels/2?viewAs=Instructor"
+    },
+    {
         title: "Scratch",
         description: "",
         icon: "🐱",
