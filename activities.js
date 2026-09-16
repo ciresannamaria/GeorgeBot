f062f78f24df58f70a343705eb738c055f4db7f6
cires.annamaria <63457648+ciresannamaria@users.noreply.github.com>
2026-09-16 09:22:55 +0200

    Add more Years 1-2 coding activities

--- activities.js
+++ activities.js
@@ -50,6 +50,42 @@ const activities = [
         link: "https://studio.code.org/courses/coursea-2025/units/1/lessons/6/levels/2?viewAs=Instructor"
     },
     {
+        title: "Loops with Scrat",
+        description: "",
+        icon: "🔁",
+        category: "Coding Games",
+        yearGroups: ["years1-2"],
+        type: "external",
+        link: "https://studio.code.org/courses/coursea-2025/units/1/lessons/8/levels/1?viewAs=Instructor"
+    },
+    {
+        title: "Loops with Laurel",
+        description: "",
+        icon: "🌳",
+        category: "Coding Games",
+        yearGroups: ["years1-2"],
+        type: "external",
+        link: "https://studio.code.org/courses/coursea-2025/units/1/lessons/9/levels/2?viewAs=Instructor"
+    },
+    {
+        title: "Ocean Scene with Loops",
+        description: "",
+        icon: "🌊",
+        category: "Coding Games",
+        yearGroups: ["years1-2"],
+        type: "external",
+        link: "https://studio.code.org/courses/coursea-2025/units/1/lessons/10/levels/2?viewAs=Instructor"
+    },
+    {
+        title: "Mini-Project: On the Move with Play Lab",
+        description: "",
+        icon: "🎮",
+        category: "Coding Games",
+        yearGroups: ["years1-2"],
+        type: "external",
+        link: "https://studio.code.org/courses/coursea-2025/units/1/lessons/12/levels/1?viewAs=Instructor"
+    },
+    {
         title: "Scratch",
         description: "",
         icon: "🐱",
